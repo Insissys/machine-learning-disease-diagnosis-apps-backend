a364c685ae2237aa016d7bd428efa4bc95781e9a
sefa <erzan@code.id>
2025-07-02 00:18:40 +0700

    add migration

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,8 @@
 # Replace env vars in config template
 envsubst < ./internal/config/config.yml.template > ./internal/config/config.yml
 
+# Run DB migration binary
+./migrate
+
 # Run the Go app
 exec ./server
