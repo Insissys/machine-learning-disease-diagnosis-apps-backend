89b6e09ced81ab38598e829422a624e1dda64cd8
sefa <erzan@code.id>
2026-02-10 15:01:21 +0700

    cors

--- internal/router/router.go
+++ internal/router/router.go
@@ -1,6 +1,7 @@
 package router
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/sefazi/machine-learning-disease-diagnosis-apps-backend/pkg/handler"
 	"github.com/sefazi/machine-learning-disease-diagnosis-apps-backend/pkg/middleware"
@@ -8,6 +9,8 @@ import (
 
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
+
+	r.Use(cors.Default())
 
 	api := r.Group("/api")
 
